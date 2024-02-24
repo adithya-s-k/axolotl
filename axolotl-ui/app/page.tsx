2a23e063af04304fbfdd0d58b2f95b4d02089063
Adithya S K <adithyaskolavi@gmail.com>
2024-02-24 16:58:11 +0530

    setup axolotl-ui project - Adithya S K

---
+++ axolotl-ui/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center ">
+      <h1 className="font-bold">Axolotl UI</h1>
+      <h2>A easy way to generate YAML for fine-tuning using axolotl</h2>
+    </main>
+  );
+}
